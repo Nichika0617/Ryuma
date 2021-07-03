2b81e0a715294bd4aefce23d6f097dfba9f835bf
SC-Bee <e195743@u-ryukyu.ac.jp>
2021-07-03 19:13:36 +0900

    add no.21,24 "パスワードをお忘れの方","メールを送信しました"

--- pages/forget/index.vue
+++ pages/forget/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="container">
-    21
-    <el-card class="forgot-pass-card">
+    21,24
+    <el-card class="forgot-pass-card" v-if="show">
       <div slot="header" class="pass-missd-header">
         <div class="pass-missed-title">パスワードをお忘れの方</div>
       </div>
       <el-input placeholder="ご登録されたメールアドレス" v-model="registered_mail_address"></el-input>
       <p class="password-reset-info">ご登録されたメールアドレスにパスワード再設定のご案内が送信されます。</p>
-      <el-button type="success" class="btn-wrapper">送信する</el-button>
+      <el-button @click="show=!show" type="success" class="btn-wrapper">送信する</el-button>
+    </el-card>
 
+    <el-card class="sent-mail-card" v-if="!show">
+      <div slot="header" class="sent-mail-header">
+        <div class="sent-mail-title">メールを送信しました</div>
+      </div>
+    <p>{{ for_confirm_mail }}へメールを送信しました。メールに記載されたURLをクリックして、パスワードの変更を完了してください。
+       メールが届かない場合、再度送信してください。</p>
+       <el-button @click="show=!show" type="success" class="btn-wrapper">再度送信する</el-button>
     </el-card>
 
   </div>
@@ -18,7 +26,9 @@ export default {
   data () {
     return {
       registered_mail_address: '',
+      for_confirm_mail: 'ryudaineko@ie.u-ryukyu.ac.jp',
       
+      show: true
     }
   },
   head(){
@@ -50,6 +60,21 @@ export default {
   }
 
   .pass-missed-title {
+    font-size: 22px;
+    text-align: center;
+    font-weight: 800;
+  }
+  
+  .btn-wrapper {
+    margin-top: 20px;
+  }
+
+  .sent-mail-card {
+    max-width: 500px;
+    margin: 0 auto;
+  }
+
+  .sent-mail-title {
     font-size: 22px;
     text-align: center;
     font-weight: 800;
