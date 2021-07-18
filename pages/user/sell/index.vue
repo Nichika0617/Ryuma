bd5ca920ea40f15693149891bb78cf4642542bba
SC-Bee <e195743@u-ryukyu.ac.jp>
2021-07-18 21:06:27 +0900

    fix sell page

--- pages/user/sell/index.vue
+++ pages/user/sell/index.vue
@@ -3,6 +3,41 @@
     6,出品入力
     <el-card class="sell-card">
       <el-form :model="SellForm">
+        <p>出品画像</p>
+        <el-upload
+          style="text-align: center;"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          :auto-upload="false"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleRemove">
+            <i slot="default" class="el-icon-plus"></i>
+            <div slot="file" slot-scope="{file}">
+              <img
+                class="el-upload-list__item-thumbnail"
+                :src="file.url" alt=""
+              >
+              <span class="el-upload-list__item-actions">
+                <span
+                  class="el-upload-list__item-preview"
+                  @click="handlePictureCardPreview(file)"
+                >
+                  <i class="el-icon-zoom-in"></i>
+                </span>
+                <span
+                  v-if="!disabled"
+                  class="el-upload-list__item-delete"
+                  @click="handleRemove(file)"
+                >
+                  <i class="el-icon-delete"></i>
+                </span>
+              </span>
+            </div>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
+
         <div style="margin-bottom: 20px;">
           <p class="input-name">商品名</p>
           <el-input
@@ -62,6 +97,11 @@
 export default {
   data () {
     return {
+
+      dialogImageUrl: '',
+      dialogVisible: false,
+      disabled: false,
+
       SellForm: {
         product_name: '',
         description: '',
@@ -114,8 +154,15 @@ export default {
   },
   methods: {
     handleChange(value) {
-        console.log(value);
-      }
+      console.log(value);
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
   },
   created() {
     
