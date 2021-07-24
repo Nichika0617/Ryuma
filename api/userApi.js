9586d0aa2bb524d2a28e143f66b27278e9c38237
由解　仁知翔 <e195765@ie.u-ryukyu.ac.jp>
2021-07-25 00:00:22 +0900

    regist途中

--- api/userApi.js
+++ api/userApi.js
@@ -14,6 +14,15 @@ export function getLoginInfo(){
     })
 }
 
+export function register(data){
+    return request({
+        url: "/member/members/register",
+        method: 'POST',
+        data
+        // オブジェクトの key がdataで，valueが引数のデータになる，省略の書き方．
+    })
+}
+
 export function getOpponentInfo(chatId){
     return request({
         url: '/member/members/opponentInfo',
