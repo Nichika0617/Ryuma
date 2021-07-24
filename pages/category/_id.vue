1af4883cb9b24e0d60b6e39d002c1db792e1c50c
由解　仁知翔 <e195765@ie.u-ryukyu.ac.jp>
2021-07-25 00:02:34 +0900

    v-for のkeyがうまくできない category触ったら大変なことになりましたごめんなさい．　

--- pages/category/_id.vue
+++ pages/category/_id.vue
@@ -1,8 +1,42 @@
 <template>
   <div class="container">
-    {{page}}
+    <div
+      v-for="item in page.records"
+      :key="item.id"
+    >
+      <el-row>
+        <div class="categoryName" style="margin-top:20px;">
+          <!-- <h3>{{categoryName}}</h3> -->
+        </div>
+      </el-row>
+
+      <el-row>
+        <el-col
+          :span="6"
+          v-for="product in item"
+          :key="product.id"
+        >
+          <el-card
+            :body-style="{ padding: '10px' }"
+            shadow="hover"
+            class="card-style"
+          >
+            <img
+              :src="product.coverUrl"
+              class="product-image"
+              @click="handleClickProduct(product.id)"
+            >
+            <div style="padding: 14px;" @click="handleClickProduct(product.id)">
+              <span>{{ product.name }}</span>
+              <span>¥{{ product.price }}</span>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
+
 <script>
 import {listProductsByCategoryId} from "@/api/product";
 export default {
@@ -22,14 +56,73 @@ export default {
       listProductsByCategoryId(this.categoryQuery)
         .then((res) => {
           this.page = res.data.page;
+          console.log(this.page);
+          console.log("レコード"+this.page.records)
+          
         })
         .catch((err) => {
           console.log(err);
         });
     },
+    handleClickProduct(productId) {
+      console.log(productId);
+      window.location.href="/user/"+productId;
+    }
   },
   created() {
     this.fetchData();
   },
 }
 </script>
+
+<style scoped>
+.card-style {
+    margin: 5px 5px 5px 5px;
+    height: 200px;
+    width: 200px;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .product-image {
+    display: block;
+    object-fit: cover;
+    height: 150px;
+    cursor: pointer;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both;
+  }
+
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409eff;
+  }
+
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+
+  .categoryName{
+    font-size:20px;
+    font-weight: bold;
+    background-color: rgb(185, 238, 203);
+  }
+
+  .row-hide{
+    overflow: hidden;
+    white-space: nowrap;
+    
+}
+
+</style>
