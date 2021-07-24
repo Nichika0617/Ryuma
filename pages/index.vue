9394683532578408502fc342db0f067051383b70
由解　仁知翔 <e195765@ie.u-ryukyu.ac.jp>
2021-07-25 00:01:40 +0900

    小さな修正 home

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,50 @@
 <template>
   <div class="container">
-    <div
-      v-for="item in indexData"
-      :key="'category'+item.categoryId"
-      v-if="item.products.length !== 0"
-    >
-      <el-row>
-        <el-col :span="6">
-          {{item.categoryName}}
-        </el-col>
-        <el-col :span="6">
-          <a :href="'/category/'+item.categoryId">もっと見る</a>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col
-          :span="4"
-          v-for="product in item.products"
-          :key="product.id"
-        >
-          <el-card
-            :body-style="{ padding: '10px' }"
-            shadow="hover"
-            class="card-style"
-          >
-            <img
-              :src="product.coverUrl"
-              class="product-image"
-              @click="handleClickProduct(product.id)"
+    <div v-for="item in indexData" :key="'category' + item.categoryId">
+      <div v-if="item.products.length !== 0">
+        <el-row>
+          <hr />
+          <div class="categoryName" style="margin-top: 20px">
+            <el-col :span="6">
+              {{ item.categoryName }}
+            </el-col>
+            <el-col :span="6">
+              <div style="float: right">
+                <a :href="'/category/' + item.categoryId">もっと見る</a>
+              </div>
+            </el-col>
+          </div>
+        </el-row>
+
+        <el-row class="row-hide">
+          <el-col :span="6" v-for="product in item.products" :key="product.id">
+            <el-card
+              :body-style="{ padding: '10px' }"
+              shadow="hover"
+              class="card-style"
             >
-            <div style="padding: 14px;" @click="handleClickProduct(product.id)">
-              <span>{{ product.name }}</span>
-              <span>¥{{product.price}}</span>
-            </div>
-          </el-card>
-        </el-col>
-      </el-row>
+              <img
+                :src="product.coverUrl"
+                class="product-image"
+                @click="handleClickProduct(product.id)"
+              />
+              <div
+                style="padding: 14px"
+                @click="handleClickProduct(product.id)"
+              >
+                <span>{{ product.name }}</span>
+                <span>¥{{ product.price }}</span>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getIndexData, listProductsByCategoryId} from "@/api/product";
+import { getIndexData, listProductsByCategoryId } from "@/api/product";
 export default {
   data() {
     return {
@@ -61,6 +64,7 @@ export default {
     fetchData() {
       getIndexData({ num: 4 }).then((res) => {
         this.indexData = res.data.data;
+        console.log(this.indexData);
       });
     },
     handleClick() {
@@ -70,8 +74,8 @@ export default {
     },
     handleClickProduct(productId) {
       console.log(productId);
-      window.location.href="/product/"+productId;
-    }
+      window.location.href = "/product/" + productId;
+    },
   },
 };
 </script>
@@ -82,6 +86,7 @@ export default {
   height: 200px;
   width: 200px;
 }
+
 .bottom {
   margin-top: 13px;
   line-height: 12px;
@@ -91,6 +96,7 @@ export default {
   display: block;
   object-fit: cover;
   height: 150px;
+  cursor: pointer;
 }
 
 .clearfix:before,
@@ -98,14 +104,28 @@ export default {
   display: table;
   content: "";
 }
+
 .clearfix:after {
   clear: both;
 }
+
 .el-dropdown-link {
   cursor: pointer;
   color: #409eff;
 }
+
 .el-icon-arrow-down {
   font-size: 12px;
+}
+
+.categoryName {
+  font-size: 20px;
+  font-weight: bold;
+  background-color: rgb(185, 238, 203);
+}
+
+.row-hide {
+  overflow: hidden;
+  white-space: nowrap;
 }
 </style>
