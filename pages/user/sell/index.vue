a0100361ffa91752b2bbba0f8456137e6f5e8811
SC-Bee <e195743@u-ryukyu.ac.jp>
2021-07-27 21:19:33 +0900

    fixed 出品

--- pages/user/sell/index.vue
+++ pages/user/sell/index.vue
@@ -2,64 +2,59 @@
   <div class="container">
     6,出品入力
     <el-card class="sell-card">
-      <el-form :model="SellForm" :rules="rules" ref="SellForm">
-        <p>出品画像</p>
-        <multi-upload v-model="SellForm.images"></multi-upload>
-        <el-dialog :visible.sync="dialogVisible">
-          <img width="100%" :src="dialogImageUrl" alt="">
-        </el-dialog>
+      <el-form 
+        :model="SellForm"
+        :rules="rules"
+        ref="SellForm"
+      >
+        <el-form-item label="商品画像" prop="productimage">
+          <multi-upload v-model="SellForm.images"></multi-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+        </el-form-item>
 
-        <div style="margin-bottom: 20px;">
-          <p class="input-name">商品名</p>
-          <el-input
+        <el-form-item label="商品名" prop="productname">
+          <el-input 
             placeholder="40文字まで"
-            v-model="SellForm.name"
-            clearable>
+            v-model="SellForm.name">
           </el-input>
-        </div>
+        </el-form-item>
         
-        <div style="margin-bottom: 40px;">
-          <p class="input-name">商品の説明</p>
-          <el-input
-            placeholder="1000文字以内"
-            v-model="SellForm.introduction"
-            clearable>
+        <el-form-item label="商品説明" prop="productdescribe">
+          <el-input 
+            placeholder="1000文字まで"
+            v-model="SellForm.name">
           </el-input>
-        </div>
+        </el-form-item>
 
-        <div class="block">
-          <p class="input-name">カテゴリー</p>
+        <el-form-item label="カテゴリー" prop="category">
           <el-cascader
             placeholder="選択してください"
             v-model="categoryIds"
-            :options="options"
+            :options="category_options"
             :props="optionProps">
           </el-cascader>
-        </div>
-
-        <div class="block">
-          <p class="input-name">商品の状態</p>
+        </el-form-item>
+        
+        <el-form-item label="商品の状態" prop="status">
           <el-cascader
             placeholder="選択してください"
-            v-model="SellForm.status">
+            v-model="SellForm.status"
+            :options="status_options"
+            :props="status_optionProps">
           </el-cascader>
-        </div>
+        </el-form-item>
 
-        <div style="margin-bottom: 40px;">
-          <p class="input-name">販売価格</p>
-          <span>¥</span>
+        <el-form-item label="商品価格" prop="price">
           <el-input
             placeholder="0"
-            v-model="SellForm.price"
-            clearable>
+            v-model="SellForm.price">
           </el-input>
-        </div>
-
-        <el-button @click="submitForm(SellForm)" type="success" round>出品する</el-button>
+        </el-form-item>
 
 
-
-
+        <el-button @click="submitForm('SellForm')" type="success" round>出品する</el-button>
       </el-form>
     </el-card>
   </div>
@@ -105,15 +100,53 @@ export default {
 
       categoryIds: [],
       loginInfo: "",
-      options: [],
+      category_options: [],
       optionProps: {
         value: "id",
         label: "name",
         expandTrigger: "hover",
       },
 
+      status_option: [
+      {
+        "id": 1,
+        "name": "新品未使用",
+        "children": null,
+      },
+      {
+        "id": 2,
+        "name": "未使用に近い",
+        "children": null,
+      },
+      {
+        "id": 3,
+        "name": "目立った傷や汚れなし",
+        "children": null,
+      },
+      {
+        "id": 4,
+        "name": "やや傷や汚れあり",
+        "children": null,
+      },
+      {
+        "id": 5,
+        "name": "傷や汚れあり",
+        "children": null,
+      },
+      {
+        "id": 6,
+        "name": "全体的に状態が悪い",
+        "children": null,
       }
-    },
+      ],
+
+      status_optionProps: {
+        value: "id",
+        label: "name",
+        expandTrigger: "hover",
+      },
+    };
+  },
 
   head(){
     return {
@@ -124,29 +157,53 @@ export default {
     
     fetchCategories() {
       getCategories().then((res) => {
-        this.options = res.data.categories;
+        this.category_options = res.data.categories;
       });
     },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
           alert('登録が完了しました．');
-          putup(this.SellForm).then(res => {
-            if(this.SellForm.code == 200) {
-              window.location.href = "/"
-            }
-          }).catch(err => {
-            this.$message.error("もう一度送信してください")
-          })
-        } else {
-          console.log('登録に失敗しました．入力を確認してください．');
-          return false;
+          this.putup(this.SellForm)
+      //     .then(res => {
+      //       window.location.href="/"
+      //     }).catch(err => {
+      //       this.$message.error("もう一度送信してください")
+      //     })
+      //   } else {
+      //     console.log('登録に失敗しました．入力を確認してください．');
+      //     return false;
+      //   }
+      // });
         }
-      });
+      })
     },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      },
+        resetForm(formName) {
+          this.$refs[formName].resetFields();
+
+        },
+
+        postData(formData){
+          console.log("以下を送信します\n" + JSON.stringify(formData));
+          putup(formData).then(res =>{ //このputupはimportしたAPI dataではない．
+            //成功した場合，
+              this.$message({
+              message: '商品登録しました',
+              type: 'success'
+              });
+              console.log("登録完了")
+              this.$router.push('/login');
+          }).catch((error) =>{
+            this.$message({
+              message: '登録に失敗しました．' + error,
+              type: 'error'
+            });
+            //vueを通り抜けたがエラーの場合，メール重複エラー？」　今(7/27)は非表示にしてます．44行目 
+              console.log("登録失敗")
+              this.resetForm('SellForm')
+              console.log(error)
+            })
+        },
   },
   created() {
     this.fetchCategories();
